ffe9fcf3e396569abc2b112897123a620ee15e77
juligove <jagonzalezv@sura.com.co>
2020-05-07 23:21:00 -0500

    implementing repo interface

---
+++ pkg/domain/repository/clientRepository.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"ms-client/pkg/domain/model"
+)
+
+// ClientRepository interface that establishes functions to be implemented depending on client storage
+type ClientRepository interface {
+	// Create saves a given client
+	Create(c *model.Client) error
+	// Fetch return all clients saved in storage
+	Fetch() ([]*model.Client, error)
+	// Delete remove gopher with given ID
+	Delete(ID string) error
+	// Update modify client with given ID and given new data
+	Update(ID string, c *model.Client) error
+	// FetchByID returns the client with given ID
+	FetchByID(ID string) (*model.Client, error)
+}
